71914d509e4f4e090ae6fae27c8cf11d5c0c05fb
SylvainPons <sylvainpons680@gmail.com>
2025-12-02 16:03:41 +0000

    adding Dockerfile

--- Agent_visa/start.sh
+++ Agent_visa/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source .venv/bin/activate
-streamlit run src/main.py --server.address 192.168.1.149 --server.port 8501
+streamlit run src/main.py --server.address 0.0.0.0 --server.port 8501
